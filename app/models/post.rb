9cc576af2745908553bf95c842e6fbdd82343a2a
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-09-28 13:40:37 +0300

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true 
+    validates :category, inclusion: { within: ["Fiction", "Non-Fiction"]}
+    validates :content, length: { minimum: 100 }
 end
